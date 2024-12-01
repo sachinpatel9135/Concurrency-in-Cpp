63249fdcd2cae4b9f77af1df5ffe3a1c013c7d21
sachinpatel9135 <sachin.patel.abcdef@gmail.com>
2024-12-02 02:26:49 +0530

    implemented how to create thread using different kind of methods

--- CreateThread.cpp
+++ CreateThread.cpp
@@ -15,17 +15,14 @@ public:
 };
 
 int main() {
-    for (int i = 0; i < 10; i++)
-        cout << "Display From Main Thread" << endl;
     thread t1(helloFunction);
-    // HelloFunctionObject HelloFunctionObject;
-    // thread t2(HelloFunctionObject);
-    // thread t3([] {
-    //     cout << "Hello from a lambda function" << "\n";
-    // });
-    
+    HelloFunctionObject HelloFunctionObject;
+    thread t2(HelloFunctionObject);
+    thread t3([] {
+        cout << "Hello from a lambda function" << "\n";
+    });
     t1.join();
-    // t2.join();
-    // t3.join();
+    t2.join();
+    t3.join();
     return 0;
 }
