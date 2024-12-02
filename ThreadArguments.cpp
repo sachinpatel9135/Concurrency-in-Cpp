aa33542846b8364df271565041a718c7240be6f7
sachinpatel9135 <sachin.patel.abcdef@gmail.com>
2024-12-02 16:20:58 +0530

    implemented how to pass arguments in thread

--- ThreadArguments.cpp
+++ ThreadArguments.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
 #include<thread>
+#include<chrono>
 
 using namespace std;
 
+class Sleeper{
+public:
+    Sleeper(int& i_): i{i_} {};
+    void operator() (int k) {
+        for(int j=0; j<=k; j++) {
+            this_thread::sleep_for(chrono::microseconds(100));
+            i += k;
+        }
+        cout << this_thread::get_id() << "\n";
+    }
+private:
+    int& i;
+};
+
 int main() {
-    string s = "C++11";
-    thread t1([=] {cout << s << "\n";});
+    int valSleeper = 1000;
+    thread t1(Sleeper(valSleeper),5);
     t1.join();
-    thread t2([&] {cout << s << "\n";});
-    t2.join();
+    cout << valSleeper << "\n";
     return 0;
 }
